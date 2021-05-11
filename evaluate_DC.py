b28a39815f2f0cc4e0e9912a9100bd968d6706d8
Claudiu Dan <cid1u17@soton.ac.uk>
2021-05-11 22:52:09 +0300

    fix path and add option to move tensors to gpu

--- evaluate_DC.py
+++ evaluate_DC.py
@@ -3,12 +3,16 @@ import numpy as np
 import torch
 from sklearn.linear_model import LogisticRegression
 from tqdm import tqdm
-use_gpu = torch.cuda.is_available()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device='cpu'    
 
 def get_base_class_statistics(file):
     base_means = []
     base_cov = []
     data = pickle.load(file)
+    
+    # get mean and covariances of base classes
     for key in data.keys():
         feature = torch.tensor(data[key])
         base_means.append(torch.mean(feature, axis = 0))
@@ -17,17 +21,18 @@ def get_base_class_statistics(file):
         cov = torch.from_numpy(np_cov)
         base_cov.append(cov)
     
-    torch_means = torch.zeros(len(base_means), len(base_means[0]))
-    torch_cov = torch.zeros(len(base_cov), len(base_cov[0]), len(base_cov[0][0]))
+    # convert from numpy arrays to torch tensors
+    torch_means = torch.zeros(len(base_means), len(base_means[0])).to(device)
+    torch_cov = torch.zeros(len(base_cov), len(base_cov[0]), len(base_cov[0][0])).to(device)
     for i in range(len(base_means)):
-        torch_means[i] = torch.tensor(base_means[i])
+        torch_means[i] = base_means[i].clone().detach()
         for j in range(len(base_cov[0])):
-            torch_cov[i][j] = torch.tensor(base_cov[i][j])
+            torch_cov[i][j] = base_cov[i][j].clone().detach()
     
     return torch_means, torch_cov
 
 def transform_tukey(data, lam):
-    transformed_data = torch.zeros(data.shape)
+    transformed_data = torch.zeros(data.shape).to(device)
     for i in range(len(data)):
         feature = data[i]
         if lam != 0:
@@ -37,27 +42,23 @@ def transform_tukey(data, lam):
     return transformed_data
 
 def calibrate(base_means, base_cov, feature, k, alpha):
-    distances = torch.zeros(len(base_means))
+    distances = torch.zeros(len(base_means)).to(device)
     for i in range(len(base_means)):
         distances[i] = torch.cdist(feature.unsqueeze(0), base_means[i].unsqueeze(0))
     _, indices = torch.topk(distances, k)
-    #selected_means = torch.tensor([base_means[i] for i in indices][0])
     selected_means = torch.index_select(base_means, 0, indices)
     selected_cov = torch.index_select(base_cov, 0, indices)
-    #print(selected_cov[0], '\n')
-    #print(selected_cov[1], '\n')
-    #print(selected_cov[0][0][0] + selected_cov[1][0][0])
     calibrated_mean = (torch.sum(selected_means, axis=0) + feature) / (k + 1)
     calibrated_cov = torch.sum(selected_cov, axis=0) / k + alpha
     return calibrated_mean, calibrated_cov
     
 if __name__ == '__main__':
-    # ---- data loading
+    # parameter initialization
     dataset = 'miniImagenet'
     n_shot = 1
     n_ways = 5
     n_queries = 15
-    n_runs = 5
+    n_runs = 100
     n_train = n_ways * n_shot
     n_test = n_ways * n_queries
     n_total = n_train + n_test
@@ -65,27 +66,25 @@ if __name__ == '__main__':
     alpha = 0.21
     n_generation = int(750 / n_shot)
     
+    # code from authors' github, gets data split as tasks
     import FSLTask
     cfg = {'shot': n_shot, 'ways': n_ways, 'queries': n_queries}
     FSLTask.loadDataSet(dataset)
     FSLTask.setRandomStates(cfg)
     ndatas = FSLTask.GenerateRunSet(end=n_runs, cfg=cfg)
-    print(ndatas.shape)
-    ndatas = ndatas.permute(0, 2, 1, 3).reshape(n_runs, n_total, -1)
-    labels = torch.arange(n_ways).view(1, 1, n_ways).expand(n_runs, n_shot + n_queries, 5).clone().view(n_runs,
-                                                                                                        n_total)
-    print(labels.shape)
-    # ---- Base class statistics
+    ndatas = ndatas.permute(0, 2, 1, 3).reshape(n_runs, n_total, -1).to(device)
+    labels = torch.arange(n_ways).view(1, 1, n_ways).expand(n_runs,
+                n_shot + n_queries, 5).clone().view(n_runs, n_total).to(device)
     base_means = None
     base_cov = None
-    base_features_path = "./checkpoints/%s/WideResNet28_10_S2M2_R/last/base_features.plk"%dataset
+    base_features_path = "./checkpoints/%s/base_features.plk"%dataset
     with open(base_features_path, 'rb') as input_file:
         base_means, base_cov = get_base_class_statistics(input_file)
         
     from torch.distributions.multivariate_normal import MultivariateNormal
     
     acc = []
-    print (ndatas.shape)
+    # run algorithm on each task 
     for task, labels in tqdm(zip(ndatas, labels)):
         tukey_data = transform_tukey(task, lam)
         # separate train data and labels from tests
@@ -96,25 +95,27 @@ if __name__ == '__main__':
         
         train_data = support_data.clone()
         train_labels = support_labels.clone()
+
+        # sample from calibrated distribution
         for feature, label in zip(support_data, support_labels):
             calibrated_mean, calibrated_cov = calibrate(base_means, base_cov, feature, 2, alpha)
             calibrated_distribution = MultivariateNormal(calibrated_mean, calibrated_cov)
-            sampled_data = torch.zeros(n_generation, len(calibrated_mean))
-            sampled_labels = torch.zeros(n_generation)
+            sampled_data = torch.zeros(n_generation, len(calibrated_mean)).to(device)
+            sampled_labels = torch.zeros(n_generation).to(device)
             for i in range(n_generation):
                 sampled_data[i] = calibrated_distribution.sample()
                 sampled_labels[i] = label
+                
+            # bring all the data required for training together
             train_data = torch.cat((train_data, sampled_data), dim=0)
             train_labels = torch.cat((train_labels, sampled_labels), dim=0)
         
-        classifier = LogisticRegression(max_iter=1000).fit(X=train_data, y=train_labels)
-        predicts = classifier.predict(test_data)
+        classifier = LogisticRegression(max_iter=1000).fit(X=train_data.cpu(), y=train_labels.cpu())
+        predicts = classifier.predict(test_data.cpu())
         count = 0
         for predict, true in zip(predicts, test_labels):
             if predict == true:
                 count += 1
-        print(count/len(predicts))
-        #print(support_data.shape, sampled_data.shape)
-        #train_data = torch.cat((support_data, sampled_data), dim=0)
-        #train_labels = torch.cat((support_labels, sampled_labels), dim=0)
-        #print (train_data.shape, train_labels.shape)
+        acc.append(count/len(predicts))
+    
+    print (np.mean(acc))
